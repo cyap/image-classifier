c5fe4069064ea8bb0ade3aa730d4eba91240bb17
Chester Yap <chesterdyap@gmail.com>
2018-02-08 17:19:10 -0600

    Add preliminary methods for image processing

--- ImageClassifierSite/ImageClassifierSite/views.py
+++ ImageClassifierSite/ImageClassifierSite/views.py
@@ -1,26 +1,29 @@
 from django.shortcuts import render
 
-from . import Classifier as cls
+from . import forms
+from .ImageHandler.classifier import Classifier
+from .ImageHandler import file_handler
 
 def index(request):
 
-	if request.method == 'GET':
-		return render(request, "index.html")
-	if request.method == 'POST':
+	if request.method == "GET":
+		return render(request, "index.html", {
+		})
 
-		# Get picture
-
-		# Upload
-
-		# Run through classifier
-
-		#classifier = cls.Classifier()
-		#classifier.classify_image(image)
-
-		# Output result
-
-		# Upload photo to database
-
+	if request.method == "POST":
+		image_path = host_image(image_data, location)
+		classification = classify_image(image_path)
 		return render(request, "result.html")
 
 
+	def host_image(image_data, location):
+		""" Saves image to /media/ """
+
+		# Hash doesn't persist
+		file_name = hash(image_data)
+		file_handler.save(image_data, file_name, location)
+		return os.path.join(location, file_name)
+
+	def classify_image(image_path):
+		classifier = Classifier()
+		return classifier.pipeline(image_path)
