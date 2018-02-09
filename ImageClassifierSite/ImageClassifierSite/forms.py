559ad6f7c6b8a2128311f8227ee2113385a657ab
Chester Yap <chesterdyap@gmail.com>
2018-02-09 09:39:05 -0600

    Add form for submitting images via Url

--- ImageClassifierSite/ImageClassifierSite/forms.py
+++ ImageClassifierSite/ImageClassifierSite/forms.py
@@ -1,7 +1,10 @@
 from django import forms
-from . import settings, models
+from . import models
 
 class ImageForm(forms.ModelForm):
 	class Meta:
 		model = models.Image
 		fields = ["upload"]
+
+class WebImageForm(forms.Form):
+	url = forms.URLField()
